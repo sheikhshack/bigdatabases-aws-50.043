7799bb4133b4af9e2ea171fbbb5e071a6c8cd1cc
jeroee <jeroejeroe@gmail.com>
2020-09-27 00:21:39 +0800

    Added node server with mongoDB connection

---
+++ server/model/user.model.js
@@ -0,0 +1,40 @@
+// user.model.js
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3
+    },
+    password: {
+        type: String,
+        required:true,
+        trim: true,
+        minlength: 3
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        trim: true,
+        minlength: 3
+    },
+
+}, {
+    timestamps: true,
+});
+
+const User = mongoose.model('Users', userSchema);
+
+module.exports = User;
---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { waitForDebugger } = require('inspector');
+const app = express();
+const mongoose = require('mongoose');
+const port = 5000;
+//const MongoClient = require('mongodb').MongoClient;
+
+app.use(express.json());
+
+const userRouter = require('./route/user.route');
+app.use('/user', userRouter);
+
+const uri = "mongodb+srv://jeroee:apples123@testdb.cpfwr.mongodb.net/testDb?retryWrites=true";
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
+
+app.listen(port, () => {
+   console.log(`Server is running on port: ${port}`);
+});
