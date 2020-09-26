7e4a28a5810c1ba96cd237b52ff2b910b1f96b0c
sheikhshack <redfreak97@gmail.com>
2020-09-27 00:23:25 +0800

    core: confiugured addons, added json file server

--- react_app/.eslintrc.js
+++ react_app/.eslintrc.js
@@ -1,51 +1,51 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "jest/globals": true,
-        "cypress/globals": true
+    'env': {
+        'browser': true,
+        'es6': true,
+        // "jest/globals": true,
+        // "cypress/globals": true
 
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended'
     ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
         },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+        'ecmaVersion': 2018,
+        'sourceType': 'module'
     },
-    "plugins": [
-        "react", "jest", "cypress"
+    'plugins': [
+        'react', 'cypress'
     ],
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
+    'rules': {
+        'indent': [
+            'error',
+            4
         ],
-        "linebreak-style": [
-            "error",
-            "unix"
+        'linebreak-style': [
+            'error',
+            'unix'
         ],
-        "quotes": [
-            "error",
-            "single"
+        'quotes': [
+            'error',
+            'single'
         ],
-        "semi": [
-            "error",
-            "never"
+        'semi': [
+            'error',
+            'never'
         ],
-        "eqeqeq": "error",
-        "no-trailing-spaces": "error",
-        "object-curly-spacing": [
-            "error", "always"
+        'eqeqeq': 'error',
+        'no-trailing-spaces': 'error',
+        'object-curly-spacing': [
+            'error', 'always'
         ],
-        "arrow-spacing": [
-            "error", { "before": true, "after": true }
+        'arrow-spacing': [
+            'error', { 'before': true, 'after': true }
         ],
-        "no-console": 0,
-        "react/prop-types": 0
+        'no-console': 0,
+        'react/prop-types': 0
     }
 }
