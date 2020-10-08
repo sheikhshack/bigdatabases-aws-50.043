0c5c19e440ee26c1a3c7ea3afb126f976152c0c9
taykiathong <49123732+taykiathong@users.noreply.github.com>
2020-10-09 00:42:26 +0800

    Update review.js

---
+++ server/model/review.js
@@ -0,0 +1,52 @@
+
+const Sequelize = require("sequelize");
+sequelize.define('User', {
+    // ... (attributes)
+  }, {
+    freezeTableName: true
+  });
+
+module.exports = sequelize.define("reviewerID", {
+  id: {
+    type: Sequelize.STRING(13),
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  content: Sequelize.STRING(300)
+});
+
+
+module.exports = sequelize.define("book", {
+    reviewText: {
+      type: Sequelize.INTEGER(11),
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    asin: {
+      type: Sequelize.STRING(35),
+      allowNull: false,
+      unique: true
+    },
+    overall: {
+        type: Sequelize.INTEGER(35),
+        allowNull: false,
+    },
+    reviewTime: {
+        type: Sequelize.STRING(35),
+        allowNull: false,
+        unique: true
+    },  
+    reviewerName: {
+        type: Sequelize.STRING(20),
+        allowNull: false
+    }
+    summary: {
+        type: Sequelize.STRING(20),
+        allowNull: false
+    }
+  });
+
+await sequelize.sync({ force: true });
+console.log("All models were synchronized successfully.");
