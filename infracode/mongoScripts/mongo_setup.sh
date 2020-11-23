3f2196bfeb5508bc4c7e3e96180e4fb2aaa7da3c
sheikhshack <redfreak97@gmail.com>
2020-11-23 19:53:07 +0800

    feat: added mongo bringup

---
+++ infracode/mongoScripts/mongo_setup.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+echo "---Setting Up MongoDB--- "
+
+# Download mongo
+{
+    wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list   
+    sudo apt-get update
+    sudo apt-get install -y mongodb-org
+} || {
+    # catch
+    echo "ERROR: installing mongodb"
+}
+
+# Download data
+{
+    wget https://www.dropbox.com/s/pcs7b9x70cwiki8/meta_Compiled_Count_haveTitle.json?dl=1 -O metadata.json
+} || {
+    # catch
+    echo "ERROR: downloading data"
+}
+
+{
+    sudo service mongod start
+} || {
+    sudo systemctl enable mongod
+    sudo service mongod start
+}
+
+# Set up admin
+
+while :
+do
+    if mongo localhost:27017/admin --eval 'db.createUser({ user: "jeroe", pwd: "Helloworld1@", roles: [ { role: "userAdminAnyDatabase", db: "admin" }, "readWriteAnyDatabase" ]})' ; then
+        break
+    else
+        echo "Command failed, retrying..."
+    fi
+done
+
+# Allow access from everywhere
+echo "Changing mongd.conf"
+sudo sed -i "s,\\(^[[:blank:]]*bindIp:\\) .*,\\1 0.0.0.0," /etc/mongod.conf
+sudo sh -c 'echo "security:\n  authorization : enabled" >> /etc/mongod.conf'
+sudo service mongod restart
+
+# import dataset
+{
+    echo "Importing dataset"
+    mongoimport --db metadata --collection kindle_Metadata --file metadata.json --authenticationDatabase admin --username 'jeroe' --password 'Helloworld1@'
+    # mongoimport -d isit_database_mongo -c kindle_metadata --file meta_Kindle_Store.json --authenticationDatabase admin --username 'admin' --password 'password' --legacy
+    # mongoimport -d isit_database_mongo -c categories --drop --file categories.json --authenticationDatabase admin --username 'admin' --password 'password'
+} || {
+    echo "ERROR: importing data to mongo"
+}
+
+echo "---MongoDB Set Up Finish---"
--- infracode/mongo_bringup.py
+++ infracode/mongo_bringup.py
@@ -5,10 +5,8 @@ from botocore.exceptions import ClientError
 import paramiko
 import os
 from time import sleep
-# import scp
 
-# Defaults
-UBUNTU_AMI_ID = 'ami-00ddb0e5626798373'  # Default Ubuntu 16.04 for US-East region
+UBUNTU_AMI_ID = 'ami-00ddb0e5626798373'  # Default Ubuntu 18.04 for US-East region
 SECURITY_PERMISSIONS = {
     'Server': [
         {'IpProtocol': 'tcp',
@@ -45,8 +43,7 @@ SECURITY_PERMISSIONS = {
              'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
         ]
 }
-SERVER_GROUP = 'SECURITY_GROUP_SERVER_TESTTESTTEST'
-
+SERVER_GROUP = 'SECURITY_GROUP_SERVER_MONGO'
 
 ########## Helper functions ##########
 # From https://gist.github.com/batok/2352501
@@ -79,7 +76,7 @@ try:
 
     # Ingress security group
     data = ec2.authorize_security_group_ingress(GroupId=security_group_id,
-                                                IpPermissions=SECURITY_PERMISSIONS['Server'])
+                                                IpPermissions=SECURITY_PERMISSIONS['Mongo'])
     print('Ingress Successfully Set %s' % data)
 
 except ClientError as e:
@@ -87,7 +84,7 @@ except ClientError as e:
 
 ############## Phase 2: Sorting out key pairs for SSH ##################
 # Key Pairs - https://boto3.amazonaws.com/v1/documentation/api/latest/guide/ec2-example-key-pairs.html
-key_name_provided = 'BOTO_TEST_RUN_v11'
+key_name_provided = 'BOTO_TEST_MONGO'
 try:
     curr_key = ec2.describe_key_pairs(KeyNames=[key_name_provided])
     print('\nKey Pair found. Moving Forwards .....')
@@ -105,45 +102,37 @@ except ClientError:  # means it doesnt exit
 # Managing EC2 Instances - https://boto3.amazonaws.com/v1/documentation/api/latest/guide/ec2-example-managing-instances.html
 # Additonal Docs - https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html#EC2.Subnet.create_instances
 # The following are Subnet class instances for provisioning (from docs)
-webserver_instance = ec2_res.create_instances(
+mongo_instance = ec2_res.create_instances(
     ImageId=UBUNTU_AMI_ID,
-    InstanceType='t2.small',
+    InstanceType='t2.large',
     SecurityGroups=[SERVER_GROUP],
     MaxCount=1,
     MinCount=1,
     KeyName=key_name_provided
 )
 
-print('{}: Provisioning and setting up instance'.format(webserver_instance[0].id))
-webserver_instance[0].wait_until_exists()
-print('{}: Server setted up and provisioned. Awaiting Run'.format(webserver_instance[0].id))
-webserver_instance[0].wait_until_running()
-print('{}: Success! Server running and ready!'.format(webserver_instance[0].id))
+print('{}: Provisioning and setting up instance'.format(mongo_instance[0].id))
+mongo_instance[0].wait_until_exists()
+print('{}: Server setted up and provisioned. Awaiting Run'.format(mongo_instance[0].id))
+mongo_instance[0].wait_until_running()
+print('{}: Success! Server running and ready!'.format(mongo_instance[0].id))
 
 
 ############## Phase 4: Setting up within the instance ##################
-webserver_instance[0].load()
-print('{0}: Success! Server currently on IP address {1}'.format(webserver_instance[0].id,webserver_instance[0].public_dns_name))
-sleep(50)
+mongo_instance[0].load()
+print('{0}: Success! Server currently on IP address {1}'.format(mongo_instance[0].id,mongo_instance[0].public_dns_name))
+sleep(20)
 
-# Command for settling ssh nonsenses
-# get_repo_access_routine = [
-#     "touch ~/.ssh/id_ed25519 ",
-#     "echo -e \"Host github.com-repo-0\n\tHostname github.com\n\tIdentityFile=/home/ubuntu/.ssh/id_ed25519\" > ~/.ssh/config",
-#     "echo -e \"-----BEGIN OPENSSH PRIVATE KEY-----\nb3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW\nQyNTUxOQAAACDGXyBqutfc5SYhs1wx5jNNFaYJ654wQDcuO2aT2TR/6wAAAJjxK4M58SuD\nOQAAAAtzc2gtZWQyNTUxOQAAACDGXyBqutfc5SYhs1wx5jNNFaYJ654wQDcuO2aT2TR/6w\nAAAEDfEJz1pkNo/+PCoKmWkBJ3A/yLWCfDLhwEVXqKHsir6sZfIGq619zlJiGzXDHmM00V\npgnrnjBANy47ZpPZNH/rAAAAFHJlZGZyZWFrOTdAZ21haWwuY29tAQ==\n-----END OPENSSH PRIVATE KEY-----\" > ~/.ssh/id_ed25519",
-#     "cd ~; git clone -b master --depth 1 git@github.com-repo-0:sheikhshack/bigdatabases-aws-50.043.git"
-#
-# ]
-webserver_routine = [
-    "cd ~; wget https://www.dropbox.com/s/8luth3csksbsbct/buildimage.tar.gz?dl=1 -O - | tar -xz ",
-    "sed -i 's_<SQLIP>_'54.205.94.117'_g;s_<MONGODBURI>_'mongodb+srv://jeroee:jerokok97@testdb.cpfwr.mongodb.net/testDb?retryWrites=true'_g;s_<MONGODBURILOG>_'mongodb+srv://jeroee:jerokok97@testdb.cpfwr.mongodb.net/testDb?retryWrites=true'_g' server/.env",
-    "wget -O - https://www.dropbox.com/s/cuu04w8mtmt5yc5/server_init.sh | bash"
+mongo_routine = [
+    "wget https://www.dropbox.com/s/mrzi4x03kkh4i19/mongo_setup.sh?dl=1 -O mongo_setup.sh",
+    "chmod +x mongo_setup.sh",
+    "./mongo_setup.sh"
 
 ]
-c = setup_ssh_client(key_name_provided+'.pem', webserver_instance[0].public_dns_name)
+c = setup_ssh_client(key_name_provided+'.pem', mongo_instance[0].public_dns_name)
 
 
-for command in webserver_routine:
+for command in mongo_routine:
     print("Executing {}".format( command ))
     stdin , stdout, stderr = c.exec_command(command)
     print(stdout.read().decode('utf=8'))
@@ -151,8 +140,4 @@ for command in webserver_routine:
     print(stderr.read().decode('utf=8'))
 c.close()
 
-print('(+) ---- Successfully deployed server at: {0}:5000'.format(webserver_instance[0].public_dns_name))
-
-
-
-
+print('(+) ---- Successfully deployed server at: {0}:5000'.format(mongo_instance[0].public_dns_name))
